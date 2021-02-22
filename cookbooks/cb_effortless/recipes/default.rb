072764e16b959a8af67d15a25cf3a43e7a812d0d
Collin McNeese <cmcneese@chef.io>
2021-02-22 10:52:12 -0600

    0.2.3
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- cookbooks/cb_effortless/recipes/default.rb
+++ cookbooks/cb_effortless/recipes/default.rb
@@ -12,13 +12,18 @@
 #   name: exec_block1
 #   command: echo 'Cookbook has run and done things.'
 
+
+chef_effortless = if Chef::CHEF_ROOT.include?("hab/pkgs/chef/chef")
+                    Chef::Config['chef_server_url'].include?("chefzero://") || nil
+                  end
+
 file '/tmp/tmpfile1'
 file '/tmp/should-be-created' unless windows?
 execute "echo 'Cookbook has run and done things.'"
 ruby_block 'show_chef_config' do
   block do
-    puts
-    puts Chef::Config
+    Chef::Config.keys.sort.each { |k| puts "#{k} -> #{Chef::Config[k]}" }
+    puts "My Effortless status is: #{chef_effortless}"
   end
   action :run
 end
