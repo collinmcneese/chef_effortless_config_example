22b68d0877f647e01222dd20dfc49d8dd1900391
Collin McNeese <cmcneese@chef.io>
2021-06-15 16:57:12 -0500

    0.2.4
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,8 +1,7 @@
 pkg_name=effortless_config_example
 pkg_origin=collinmcneese
-pkg_version="0.2.3"
+pkg_version="0.2.4"
 pkg_maintainer="Collin McNeese <cmcneese@chef.io>"
 pkg_license=("Apache-2.0")
 pkg_scaffolding="chef/scaffolding-chef-infra"
 scaffold_policy_name="effortless_config_example"
-
